a4959d8ae78c6dcee091fd3ff7a006ded4f863d8
ClaraEnriquez26 <89706814+ClaraEnriquez26@users.noreply.github.com>
2022-06-02 22:48:38 -0300

    Add files via upload

---
+++ src/components/Cartwidget.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+const CartWidget = ()  => {
+    return (
+      <>
+         <div className='logoCarrito'> 
+            <div className='numeroCarrito'>
+              6
+            </div>
+          <i className="bi bi-cart"></i>
+        </div> 
+      </>       
+    );
+  }
+
+  export default CartWidget;
+  
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+
+
+const ItemListContainer = ({greetings})  => {   
+    return (
+         <div>
+           <h4>{greetings}</h4>
+         </div>
+    )
+}
+
+export default ItemListContainer;
